40445e423b0eb860f6792dff41def174bcc882e7
Megha Khatiwala <khatiwalamegha@gmail.com>
2020-01-01 20:00:58 -0600

    final commit

--- lesson1/mission-planning-dashbord/src/app/equipment/equipment.component.ts
+++ lesson1/mission-planning-dashbord/src/app/equipment/equipment.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipments: string[]=["Habitat dome","Drones","Food containers","Oxygen tanks"];
+  equipmentBeingEdited: string = "";
+ 
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(Equipment: string) {
+
+    if (this.equipments.indexOf(Equipment) > 0) {
+      //Do nothing
+    } else {
+    this.equipments.push(Equipment);
+    }
+  }
+
+  remove(equipment: string) {
+    let index = this.equipments.indexOf(equipment);
+    this.equipments.splice(index, 1);
+  }
+  
+  edit(Equipment: string) {
+    this.equipmentBeingEdited = Equipment;
+  }
+ 
+ save(name: string, index: number, equipments: string[]) {
+   this.equipments[index] = name;
+   this.equipmentBeingEdited = "";
+ }
+
 
 }
--- lesson1/mission-planning-dashbord/src/app/experiments/experiments.component.ts
+++ lesson1/mission-planning-dashbord/src/app/experiments/experiments.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[]= ["Mars soil sample","Plant growth in habitat","Human bone density"];
+  experimentsBeingEdited: string="";
+
   constructor() { }
 
   ngOnInit() {
   }
+  add(Experiments: string) {
+    this.experiments.push(Experiments);
+  }
+  remove(experiments: string) {
+    let index = this.experiments.indexOf(experiments);
+    this.experiments.splice(index, 1);
+  }
+  edit(Experiments: string) {
+    this.experimentsBeingEdited = Experiments;
+  }
+  save(name: string, index: number, experiments: string[]) {
+    this.experiments[index] = name;
+    this.experimentsBeingEdited = "";
+  }
+ 
 
 }
